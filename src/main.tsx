a433b38d3dd240e3157db1b3fa2c265ae3b56650
fajar-dev <fajarrivaldi2015@gmail.com>
2025-08-17 16:16:36 +0700

    init: i18next multi language

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
--- src/pages/LabelGenerator.tsx
+++ src/pages/LabelGenerator.tsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, CssBaseline, Stack, Button, FormControl, MenuItem, Select } from "@mui/material";
 import ShippingDetailForm from "../components/ShippingDetailForm";
 import ShipperForm from "../components/ShipperForm";
 import ConsigneeForm from "../components/ConsigneeForm";
+import { useTranslation } from "react-i18next";
 
 const LabelGenerator: React.FC = () => {
+  const { t, i18n } = useTranslation(); 
+  const [lang, setLang] = React.useState("id");
+
+  useEffect(() => {
+    const savedLang = localStorage.getItem("lang");
+    if (savedLang) {
+      setLang(savedLang);
+      i18n.changeLanguage(savedLang);
+    }
+  }, [i18n]);
+
+  const handleChangeLang = (event: any) => {
+    const newLang = event.target.value;
+    setLang(newLang);
+    i18n.changeLanguage(newLang);
+    localStorage.setItem("lang", newLang);
+  };
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="xl" className="py-8">
         <section className="relative mb-6">
-          <h1 className="text-4xl font-medium text-center">Shipping Label Generator</h1>
+          <h1 className="text-4xl font-medium text-center">
+            {t("title")}
+          </h1>
 
           <div className="absolute right-0 top-0">
             <FormControl sx={{ minWidth: 120 }} size="small">
-              <Select defaultValue="id">
+              <Select value={lang} onChange={handleChangeLang}>
                 <MenuItem value="id">Bahasa</MenuItem>
                 <MenuItem value="en">English</MenuItem>
               </Select>
@@ -28,8 +49,8 @@ const LabelGenerator: React.FC = () => {
 
         <section className="flex justify-end">
           <Stack spacing={2} direction="row">
-            <Button variant="outlined" size="large">Preview</Button>
-            <Button variant="contained" size="large">Buat Label</Button>
+            <Button variant="outlined" size="large">{t("preview")}</Button>
+            <Button variant="contained" size="large">{t("createLabel")}</Button>
           </Stack>
         </section>
       </Container>
@@ -37,4 +58,4 @@ const LabelGenerator: React.FC = () => {
   );
 };
 
-export default LabelGenerator
+export default LabelGenerator;
